c5b18aaa56ebf1fdc0c151048f5f5580dcd6e39a
ColeR1 <crichards24@student.dimanregional.org>
2023-09-15 10:25:22 -0400

    gridmovement

---
+++ Assets/Scripts/Player/GridMap.cs
@@ -0,0 +1,126 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridMap : MonoBehaviour
+{
+    public int minGridSize = 3;             // Minimum grid size
+    public int maxGridSize = 10;            // Maximum grid size
+    public GameObject gridCellPrefab;       // Prefab for the grid cell
+    public GameObject mountainPrefab;       // Prefab for the mountain object
+    public GameObject newPrefab;          //Prefab for hte forest object
+    public GameObject borderBlockPrefab;    // Prefab for the border block
+    public GameObject playerPrefab;         // Prefab for the player
+    private int gridWidth;
+    private int gridHeight;
+    private bool[,] grid; // 2D array to represent the grid with blocked cells
+
+    public movementtest movement;
+
+
+    private float tileSize = 2.1f; // Size of each grid cell
+
+
+ void Start()
+    {
+        gridWidth = Random.Range(minGridSize, maxGridSize + 1);
+        gridHeight = Random.Range(minGridSize, maxGridSize + 1);
+        grid = new bool[gridWidth, gridHeight];
+
+        GenerateRandomGrid();
+
+          // Instantiate the player at the center of the grid
+        InstantiatePlayer();
+    }
+
+    void GenerateRandomGrid()
+    {
+        // Create border blocks
+        CreateBorderBlocks();
+
+        for (int x = 1; x < gridWidth - 1; x++)
+    {
+        for (int y = 1; y < gridHeight - 1; y++)
+        {
+            // Instantiate grid cell, mountain, or the new prefab based on a condition
+            GameObject cellPrefab;
+            float randomValue = Random.value;
+            if (randomValue < 0.1f)
+            {
+                cellPrefab = mountainPrefab;
+            }
+            else if (randomValue < 0.2f)
+            {
+                cellPrefab = newPrefab; // Use the new prefab when randomValue is between 0.1f and 0.2f
+            }
+            else
+            {
+                cellPrefab = gridCellPrefab;
+            }
+
+            Vector3 cellPosition = new Vector3(x * 2.1f, 0f, y * 2.1f);
+            GameObject gridCell = Instantiate(cellPrefab, cellPosition, Quaternion.identity);
+            gridCell.transform.parent = transform;
+
+            // Update the grid array to mark blocked cells
+            grid[x, y] = (cellPrefab == mountainPrefab);
+        }
+    }
+    }
+
+    void CreateBorderBlocks()
+    {
+        for (int x = 0; x < gridWidth; x++)
+        {
+            for (int y = 0; y < gridHeight; y++)
+            {
+                if (x == 0 || x == gridWidth - 1 || y == 0 || y == gridHeight - 1)
+                {
+                    // Create a border block with a collider
+                    Vector3 blockPosition = new Vector3(x * 2.1f, 0f, y * 2.1f);
+                    GameObject borderBlock = Instantiate(borderBlockPrefab, blockPosition, Quaternion.identity);
+                    borderBlock.transform.parent = transform;
+
+                    // Add a collider to the border block
+                    borderBlock.AddComponent<BoxCollider>();
+                }
+            }
+        }
+    }
+
+        bool IsCollidingWithBorderBlock(Vector3 position)
+         {
+        Collider[] colliders = Physics.OverlapSphere(position, 0.1f); // Adjust the radius as needed
+        foreach (Collider collider in colliders)
+        {
+            if (collider.CompareTag("borderblock"))
+            {
+                return true;
+            }
+        }
+        return false;
+        }
+
+    void InstantiatePlayer()
+        {
+       // Calculate a random position within the grid
+    int randomX = Random.Range(1, gridWidth - 1);
+    int randomY = Random.Range(1, gridHeight - 1);
+
+    // Calculate the position with the Y-axis offset
+    float xOffset = randomX * 2.1f;
+    float yOffset = 1.5f;
+    float zOffset = randomY * 2.1f;
+    
+    Vector3 playerSpawnPosition = new Vector3(xOffset, yOffset, zOffset);
+
+    // Instantiate the player at the adjusted position
+    GameObject player = Instantiate(playerPrefab, playerSpawnPosition, Quaternion.identity);
+    movement = player.GetComponent<movementtest>();
+
+    // Set the player's grid and enable movement
+    movement.SetGrid(grid);
+    movement.EnableMovement();
+        }
+}
+
---
+++ Assets/Scripts/Player/movementtest.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementtest : MonoBehaviour
+{ 
+   public float moveSpeed = 5.0f;  // Movement speed
+    public float tileSize = 2.1f;   // Size of each grid tile
+    private int gridWidth;
+    private int gridHeight;
+    private bool[,] grid; // Reference to the grid with blocked cells
+
+    private Vector3 targetPosition;
+
+    private bool isMoving = false; // Flag to check if the player is currently moving
+    private bool canMove = true;  // Flag to control player movement
+
+    void Start()
+    {
+        targetPosition = transform.position;
+    }
+
+    void Update()
+    {
+        if (canMove && !isMoving)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                TryMove(Vector3.left);
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                TryMove(Vector3.right);
+            }
+            else if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                TryMove(Vector3.forward);
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                TryMove(Vector3.back);
+            }
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+    }
+
+    void TryMove(Vector3 direction)
+    {
+        Vector3 newTargetPosition = targetPosition + direction * tileSize;
+
+        // Calculate the grid cell indices for the new position
+        int x = Mathf.FloorToInt(newTargetPosition.x / tileSize);
+        int y = Mathf.FloorToInt(newTargetPosition.z / tileSize);
+
+        // Check if the new position is within grid bounds, not blocked, and not on a border block
+        if (IsValidMove(x, y) && !IsOnBorderBlock(newTargetPosition))
+        {
+            StartCoroutine(MoveCoroutine(newTargetPosition));
+        }
+    }
+
+    bool IsValidMove(int x, int y)
+    {
+        // Check if the new position is within the grid bounds and not blocked
+        return x >= 0 && x < gridWidth && y >= 0 && y < gridHeight && !grid[x, y];
+    }
+
+    bool IsOnBorderBlock(Vector3 position)
+    {
+        // Perform a raycast to check if the player is on a border block
+        RaycastHit hit;
+        if (Physics.Raycast(position + Vector3.up * 0.5f, Vector3.down, out hit))
+        {
+            return hit.collider.CompareTag("borderblock"); // Adjust the tag as needed
+        }
+
+        return false;
+    }
+
+    IEnumerator MoveCoroutine(Vector3 destination)
+    {
+        isMoving = true; // Set the flag to indicate that the player is moving
+
+        float journeyLength = Vector3.Distance(targetPosition, destination);
+        float startTime = Time.time;
+
+        while (Time.time < startTime + 0.3f)
+        {
+            float distanceCovered = (Time.time - startTime) * moveSpeed;
+            float fractionOfJourney = distanceCovered / journeyLength;
+
+            targetPosition = Vector3.Lerp(targetPosition, destination, fractionOfJourney);
+
+            yield return null;
+        }
+
+        targetPosition = destination;
+        isMoving = false; // Reset the flag to indicate that the player has stopped moving
+        yield return null;
+    }
+
+    public void SetGrid(bool[,] newGrid)
+    {
+        grid = newGrid;
+        gridWidth = grid.GetLength(0);
+        gridHeight = grid.GetLength(1);
+    }
+
+    // Call this function to enable player movement
+    public void EnableMovement()
+    {
+        canMove = true;
+    }
+
+    // Call this function to disable player movement
+    public void DisableMovement()
+    {
+        canMove = false;
+    }
+}
